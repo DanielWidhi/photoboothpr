33bd7b0b3c323e7dd5f05f402902c82956b84be2
Daniel Widhi <danielwidhi98@gmail.com>
2025-08-06 12:28:56 +0800

    update nama db hosting

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,11 @@
 <?php
 // Database configuration
 $servername = "localhost"; // Change to your server name
-$username = "root"; // Change to your database username
-// $username = "u140154479_photobooth"; // Change to your database username
-$password = ""; // Change to your database password
-// $password = "Photobooth123@"; // Change to your database password
-$database = "u140154479_photobooth"; // Change to your database name
+// $username = "root"; // Change to your database username
+$username = "gdpd2131_u140154479_photobooth"; // Change to your database username
+$password = "u140154479_photobooth"; // Change to your database password
+// $password = "gdpd2131_u140154479_photobooth"; // Change to your database password
+$database = "gdpd2131_u140154479_photobooth"; // Change to your database name
 
 // Create connection
 $koneksi = new mysqli($servername, $username, $password, $database);
