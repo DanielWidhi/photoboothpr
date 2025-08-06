ed2911598733304443a1534a9a933d868daf704c
Daniel Widhi <danielwidhi98@gmail.com>
2025-08-06 14:52:17 +0800

    update user db pass

--- koneksi.php
+++ koneksi.php
@@ -2,8 +2,8 @@
 // Database configuration
 $servername = "localhost"; // Change to your server name
 // $username = "root"; // Change to your database username
-$username = "gdpd2131_u140154479_photobooth"; // Change to your database username
-$password = "u140154479_photobooth"; // Change to your database password
+$username = "gdpd2131_gdpbooth"; // Change to your database username
+$password = "gdpbooth"; // Change to your database password
 // $password = "gdpd2131_u140154479_photobooth"; // Change to your database password
 $database = "gdpd2131_u140154479_photobooth"; // Change to your database name
 
